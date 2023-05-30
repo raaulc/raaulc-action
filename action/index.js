8faa8e7ef7a12b33ed493796335fd1edbb41700d
Rahul Rathod <65287267+raaulc@users.noreply.github.com>
2023-05-30 13:50:20 +0100

    Updated version to 1.0.11 again

--- action/index.js
+++ action/index.js
@@ -1,18 +1,5 @@
 const core = require('@actions/core');
 const axios = require('axios');
-async function getGitHubUserInfo() {
-  const token = process.env.GITHUB_TOKEN;
-  const headers = {
-    Authorization: `Bearer ${token}`,
-    Accept: 'application/vnd.github.v3+json',
-  };
-  try {
-    const response = await axios.get('https://api.github.com/user', { headers });
-    return response.data;
-  } catch (error) {
-    throw new Error(`Failed to fetch GitHub user info: ${error.message}`);
-  }
-}
 try {
   const slackWebhook = process.env.SLACK_WEBHOOK;
   const slackMessage = process.env.SLACK_MESSAGE;
@@ -27,46 +14,46 @@ try {
   const githubRunId = process.env.GITHUB_RUN_ID;
   const githubActionUrl = `https://github.com/${githubRepo}/actions/runs/${githubRunId}`;
   const githubCommitUrl = `https://github.com/${githubRepo}/commit/${process.env.GITHUB_SHA}`;
-  getGitHubUserInfo().then((userInfo) => {
-    const profilePicUrl = userInfo.avatar_url;
-    const payload = {
-      username: 'rahul-action-bot',
-      icon_url: profilePicUrl,
-      attachments: [
-        {
-          color: 'good', // Use 'good' for green color
-          author_name: 'raaulc',
-          fields: [
-            {
-              title: 'Ref',
-              value: `refs/heads/${branchName}`,
-              short: true,
-            },
-            {
-              title: 'Event',
-              value: eventName,
-              short: true,
-            },
-            {
-              title: 'Actions URL',
-              value: `<${githubActionUrl}|ci-pipeline>`,
-              short: true,
-            },
-            {
-              title: 'Commit',
-              value: `<${githubCommitUrl}|${commitId}>`,
-              short: true,
-            },
-          ],
-          footer: "Powered By rtCamp's GitHub Actions Library",
-        },
-      ],
-    };
-    axios.post(slackWebhook, payload);
-    console.log('Slack notification sent successfully');
-  }).catch((error) => {
-    core.setFailed(`Failed to send Slack notification: ${error.message}`);
-  });
+  const payload = {
+    username: 'rahul-action-bot',
+    icon_url: 'https://example.com/user-photo.png', // Replace with the actual user photo URL
+    attachments: [
+      {
+        color: 'good', // Use 'good' for green color
+        author_name: 'raaulc',
+        fields: [
+          {
+            title: 'Ref',
+            value: `refs/heads/${branchName}`,
+            short: true,
+          },
+          {
+            title: 'Event',
+            value: eventName,
+            short: true,
+          },
+          {
+            title: 'Actions URL',
+            value: `<${githubActionUrl}|ci-pipeline>`,
+            short: true,
+          },
+          {
+            title: 'Commit',
+            value: `<${githubCommitUrl}|${commitId}>`,
+            short: true,
+          },
+          {
+            title: 'Message',
+            value: slackMessage,
+            short: false,
+          },
+        ],
+        footer: 'Powered By rtCamp\'s GitHub Actions Library',
+      },
+    ],
+  };
+  axios.post(slackWebhook, payload);
+  console.log('Slack notification sent successfully');
 } catch (error) {
   core.setFailed(`Failed to send Slack notification: ${error.message}`);
 }

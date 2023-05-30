b8aafa1f8a81b10aca3f4254c107633e666dce5a
Rahul Rathod <65287267+raaulc@users.noreply.github.com>
2023-05-30 14:03:47 +0100

    Added Giphy

--- action/index.js
+++ action/index.js
@@ -1,67 +1,70 @@
 const core = require('@actions/core');
 const axios = require('axios');
-try {
-  const slackWebhook = process.env.SLACK_WEBHOOK;
-  const slackMessage = process.env.SLACK_MESSAGE;
-  const authorName = process.env.GITHUB_ACTOR;
-  const commitId = process.env.GITHUB_SHA.slice(0, 4); // Shorten commit ID to 4 digits
-  const commitMsg = process.env.GITHUB_EVENT_PATH
-    ? require(process.env.GITHUB_EVENT_PATH).head_commit.message
-    : '';
-  const branchName = process.env.GITHUB_REF.split('/').slice(2).join('/'); // Extract branch name from GITHUB_REF
-  const eventName = process.env.GITHUB_EVENT_NAME;
-  const githubRepo = process.env.GITHUB_REPOSITORY;
-  const githubRunId = process.env.GITHUB_RUN_ID;
-  const githubActionUrl = `https://github.com/${githubRepo}/actions/runs/${githubRunId}`;
-  const githubCommitUrl = `https://github.com/${githubRepo}/commit/${process.env.GITHUB_SHA}`;
-  const giphyApiKey = process.env.GIPHY_API_KEY; // Add your Giphy API key here
-  const giphyUrl = `https://api.giphy.com/v1/gifs/random?api_key=${giphyApiKey}&tag=office`; // Customize the tag as per your preference
-  // Fetch a random GIF from Giphy
-  const response = await axios.get(giphyUrl);
-  const gifUrl = response.data.data.image_url;
-  const payload = {
-    username: 'rahul-action-bot',
-    icon_url: 'https://example.com/user-photo.png', // Replace with the actual user photo URL
-    attachments: [
-      {
-        color: 'good', // Use 'good' for green color
-        author_name: 'raaulc',
-        fields: [
-          {
-            title: 'Ref',
-            value: `refs/heads/${branchName}`,
-            short: true,
-          },
-          {
-            title: 'Event',
-            value: eventName,
-            short: true,
-          },
-          {
-            title: 'Actions URL',
-            value: `<${githubActionUrl}|ci-pipeline>`,
-            short: true,
-          },
-          {
-            title: 'Commit',
-            value: `<${githubCommitUrl}|${commitId}>`,
-            short: true,
-          },
-          {
-            title: 'Message',
-            value: slackMessage,
-            short: false,
-          },
-        ],
-        footer: 'Powered By rtCamp\'s GitHub Actions Library',
-      },
-      {
-        image_url: gifUrl, // Add the Giphy GIF URL here
-      },
-    ],
-  };
-  await axios.post(slackWebhook, payload);
-  console.log('Slack notification sent successfully');
-} catch (error) {
-  core.setFailed(`Failed to send Slack notification: ${error.message}`);
+async function run() {
+  try {
+    const slackWebhook = process.env.SLACK_WEBHOOK;
+    const slackMessage = process.env.SLACK_MESSAGE;
+    const authorName = process.env.GITHUB_ACTOR;
+    const commitId = process.env.GITHUB_SHA.slice(0, 4); // Shorten commit ID to 4 digits
+    const commitMsg = process.env.GITHUB_EVENT_PATH
+      ? require(process.env.GITHUB_EVENT_PATH).head_commit.message
+      : '';
+    const branchName = process.env.GITHUB_REF.split('/').slice(2).join('/'); // Extract branch name from GITHUB_REF
+    const eventName = process.env.GITHUB_EVENT_NAME;
+    const githubRepo = process.env.GITHUB_REPOSITORY;
+    const githubRunId = process.env.GITHUB_RUN_ID;
+    const githubActionUrl = `https://github.com/${githubRepo}/actions/runs/${githubRunId}`;
+    const githubCommitUrl = `https://github.com/${githubRepo}/commit/${process.env.GITHUB_SHA}`;
+    const giphyApiKey = process.env.GIPHY_API_KEY; // Add your Giphy API key here
+    const giphyUrl = `https://api.giphy.com/v1/gifs/random?api_key=${giphyApiKey}&tag=office`; // Customize the tag as per your preference
+    // Fetch a random GIF from Giphy
+    const response = await axios.get(giphyUrl);
+    const gifUrl = response.data.data.image_url;
+    const payload = {
+      username: 'rahul-action-bot',
+      icon_url: 'https://example.com/user-photo.png', // Replace with the actual user photo URL
+      attachments: [
+        {
+          color: 'good', // Use 'good' for green color
+          author_name: 'raaulc',
+          fields: [
+            {
+              title: 'Ref',
+              value: `refs/heads/${branchName}`,
+              short: true,
+            },
+            {
+              title: 'Event',
+              value: eventName,
+              short: true,
+            },
+            {
+              title: 'Actions URL',
+              value: `<${githubActionUrl}|ci-pipeline>`,
+              short: true,
+            },
+            {
+              title: 'Commit',
+              value: `<${githubCommitUrl}|${commitId}>`,
+              short: true,
+            },
+            {
+              title: 'Message',
+              value: slackMessage,
+              short: false,
+            },
+          ],
+          footer: "Powered By rtCamp's GitHub Actions Library",
+        },
+        {
+          image_url: gifUrl, // Add the Giphy GIF URL here
+        },
+      ],
+    };
+    await axios.post(slackWebhook, payload);
+    console.log('Slack notification sent successfully');
+  } catch (error) {
+    core.setFailed(`Failed to send Slack notification: ${error.message}`);
+  }
 }
+run();

748621aba277a19c2768a8b29a685ddd2def5b28
Rahul Rathod <65287267+raaulc@users.noreply.github.com>
2023-05-30 12:20:29 +0100

    Update index.js

--- action/index.js
+++ action/index.js
@@ -4,6 +4,9 @@ try {
   const slackWebhook = process.env.SLACK_WEBHOOK;
   const slackMessage = process.env.SLACK_MESSAGE;
   const authorName = process.env.GITHUB_ACTOR;
+  const commitRef = process.env.GITHUB_REF;
+  const commitEvent = process.env.GITHUB_EVENT_NAME;
+  const actionsUrl = process.env.GITHUB_WORKFLOW;
   const commitId = process.env.GITHUB_SHA;
   const commitMsg = process.env.GITHUB_EVENT_PATH
     ? require(process.env.GITHUB_EVENT_PATH).head_commit.message
@@ -15,16 +18,32 @@ try {
         author_name: authorName,
         fields: [
           {
-            title: 'Commit ID',
+            title: 'Ref',
+            value: commitRef,
+            short: true,
+          },
+          {
+            title: 'Event',
+            value: commitEvent,
+            short: true,
+          },
+          {
+            title: 'Actions URL',
+            value: actionsUrl,
+            short: false,
+          },
+          {
+            title: 'Commit',
             value: commitId,
             short: true,
           },
           {
-            title: 'Commit Message',
+            title: 'Message',
             value: commitMsg,
             short: true,
           },
         ],
+        footer: 'Powered By rtCamp\'s GitHub Actions Library',
       },
     ],
   };

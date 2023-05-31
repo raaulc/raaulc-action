9af9d091c6ecb3484fda06e327ec4e6f40ba0d2d
Rahul Rathod <65287267+raaulc@users.noreply.github.com>
2023-05-31 15:36:34 +0100

    Update index.js

--- action/index.js
+++ action/index.js
@@ -1,5 +1,96 @@
 const core = require('@actions/core');
 const axios = require('axios');
+ 
+async function run() {
+  try {
+    const slackWebhook = process.env.SLACK_WEBHOOK;
+    const slackMessage = process.env.SLACK_MESSAGE;
+    const authorName = process.env.GITHUB_ACTOR;
+    const commitId = process.env.GITHUB_SHA.slice(0, 4); // Shorten commit ID to 4 digits
+    const commitMsg = process.env.GITHUB_EVENT_PATH
+      ? require(process.env.GITHUB_EVENT_PATH).head_commit.message
+      : '';
+    const branchName = process.env.GITHUB_REF.split('/').slice(2).join('/'); // Extract branch name from GITHUB_REF
+    const eventName = process.env.GITHUB_EVENT_NAME;
+    const githubRepo = process.env.GITHUB_REPOSITORY;
+    const githubRunId = process.env.GITHUB_RUN_ID;
+    const githubActionUrl = `https://github.com/${githubRepo}/actions/runs/${githubRunId}`;
+    const githubCommitUrl = `https://github.com/${githubRepo}/commit/${process.env.GITHUB_SHA}`;
+    const giphyApiKey = "nP4SvtCMSp9XMUTtyW9WquXQfTFka5Aj"; // Add your Giphy API key here
+    const giphyUrl = `https://api.giphy.com/v1/gifs/random?api_key=${giphyApiKey}&tag=office&rating=g`;
+ 
+    // Fetch a random GIF from Giphy
+    const response = await axios.get(giphyUrl);
+    const gifUrl = response.data.data.images.fixed_width_small.url;
+    console.log("gifUrl: " + gifUrl);
+ 
+    const payload = {
+      username: 'rahul-action-bot',
+      icon_url: 'https://example.com/user-photo.png', // Replace with the actual user photo URL
+      attachments: [
+        {
+          color: 'good', // Use 'good' for green color
+          author_name: authorName,
+          fields: [
+            {
+              title: 'Ref',
+              value: `refs/heads/${branchName}`,
+              short: true,
+            },
+            {
+              title: 'Event',
+              value: eventName,
+              short: true,
+            },
+            {
+              title: 'Actions URL',
+              value: `<${githubActionUrl}|ci-pipeline>`,
+              short: true,
+            },
+            {
+              title: 'Commit',
+              value: `<${githubCommitUrl}|${commitId}>`,
+              short: true,
+            },
+            {
+              title: 'Message',
+              value: slackMessage,
+              short: false,
+            },
+          ],
+          footer: "Powered By raaulc's GitHub Actions Library",
+        },
+      ],
+    };
+ 
+    await axios.post(slackWebhook, payload);
+    console.log('Slack notification sent successfully');
+ 
+    // Send the GIF as a separate attachment
+    const gifPayload = {
+      username: 'rahul-action-bot',
+      icon_url: 'https://example.com/user-photo.png', // Replace with the actual user photo URL
+      attachments: [
+        {
+          image_url: gifUrl,
+        },
+      ],
+    };
+ 
+    await axios.post(slackWebhook, gifPayload);
+    console.log('GIF sent successfully');
+  } catch (error) {
+    core.setFailed(`Failed to send Slack notification: ${error.message}`);
+  }
+}
+ 
+run();
+ 
+
+
+/*
+const core = require('@actions/core');
+const axios = require('axios');
 async function run() {
   try {
     const slackWebhook = process.env.SLACK_WEBHOOK;
@@ -69,3 +160,4 @@ async function run() {
   }
 }
 run();
+*/
